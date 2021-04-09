a49369311d4842154f1ca19bed17da8eda3d3c08
Titus Yusinyu <titusxp@outlook.com>
2021-04-09 16:09:19 +0100

    more test code written on the angular test project

--- AngularTest/src/app/app.module.ts
+++ AngularTest/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeListComponent,
+    EmployeeDetailComponent
   ],
   imports: [
     BrowserModule
